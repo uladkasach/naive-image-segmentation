de2d1d0776897b2a47a40568c7ca0eec98615d89
UladKasach <nappa300@gmail.com>
2018-02-02 00:07:00 -0500

    create blurred images to conduct external variation for creating an roc curve for the image_segmentation project

---
+++ image_segmentation/images/source/blur.sh
@@ -0,0 +1,35 @@
+convert camel_1.jpg -blur 0x8 camel_1.blur_1.jpg
+convert camel_1.blur_1.jpg -blur 0x8 camel_1.blur_2.jpg
+convert camel_1.blur_2.jpg -blur 0x8 camel_1.blur_3.jpg
+convert camel_1.blur_3.jpg -blur 0x8 camel_1.blur_4.jpg
+convert camel_1.blur_4.jpg -blur 0x8 camel_1.blur_5.jpg
+
+convert camel_2.jpg -blur 0x8 camel_2.blur_1.jpg
+convert camel_2.blur_1.jpg -blur 0x8 camel_2.blur_2.jpg
+convert camel_2.blur_2.jpg -blur 0x8 camel_2.blur_3.jpg
+convert camel_2.blur_3.jpg -blur 0x8 camel_2.blur_4.jpg
+convert camel_2.blur_4.jpg -blur 0x8 camel_2.blur_5.jpg
+
+convert camel_3.jpg -blur 0x8 camel_3.blur_1.jpg
+convert camel_3.blur_1.jpg -blur 0x8 camel_3.blur_2.jpg
+convert camel_3.blur_2.jpg -blur 0x8 camel_3.blur_3.jpg
+convert camel_3.blur_3.jpg -blur 0x8 camel_3.blur_4.jpg
+convert camel_3.blur_4.jpg -blur 0x8 camel_3.blur_5.jpg
+
+convert camel_4.jpg -blur 0x8 camel_4.blur_1.jpg
+convert camel_4.blur_1.jpg -blur 0x8 camel_4.blur_2.jpg
+convert camel_4.blur_2.jpg -blur 0x8 camel_4.blur_3.jpg
+convert camel_4.blur_3.jpg -blur 0x8 camel_4.blur_4.jpg
+convert camel_4.blur_4.jpg -blur 0x8 camel_4.blur_5.jpg
+
+convert camel_8.jpg -blur 0x8 camel_8.blur_1.jpg
+convert camel_8.blur_1.jpg -blur 0x8 camel_8.blur_2.jpg
+convert camel_8.blur_2.jpg -blur 0x8 camel_8.blur_3.jpg
+convert camel_8.blur_3.jpg -blur 0x8 camel_8.blur_4.jpg
+convert camel_8.blur_4.jpg -blur 0x8 camel_8.blur_5.jpg
+
+convert camel_10.jpg -blur 0x8 camel_10.blur_1.jpg
+convert camel_10.blur_1.jpg -blur 0x8 camel_10.blur_2.jpg
+convert camel_10.blur_2.jpg -blur 0x8 camel_10.blur_3.jpg
+convert camel_10.blur_3.jpg -blur 0x8 camel_10.blur_4.jpg
+convert camel_10.blur_4.jpg -blur 0x8 camel_10.blur_5.jpg
